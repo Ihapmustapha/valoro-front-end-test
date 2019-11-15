9d9be4ec9b4b8557a92ce70072d7d745a3f31a2b
ihapmustapha <ihapmustapha@gmail.com>
2019-11-15 21:41:38 +0200

    Login Form Component

---
+++ src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,140 @@
+import React from "react";
+import Fab from "@material-ui/core/Fab";
+import Link from "@material-ui/core/Link";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Facebook from "../../assets/icons/facebook.svg";
+import Twitter from "../../assets/icons/twitter.svg";
+import Google from "../../assets/icons/google.svg";
+import Box from "@material-ui/core/Box";
+import Checkbox from "@material-ui/core/Checkbox";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+
+const LoginForm = ({ classes }) => {
+  return (
+    <Grid
+      container
+      item
+      xs={12}
+      sm={12}
+      md={12}
+      className={classes.formContainer}
+      direction="column"
+      alignItems="flex-start"
+      justify="center"
+    >
+      <Grid item>
+        <Typography component="h1" variant="h5" className={classes.loginHeader}>
+          Login
+        </Typography>
+      </Grid>
+      <Grid item>
+        <Typography variant="subtitle1" className={classes.loginCaption}>
+          Don't have an account?{" "}
+          <Link href="#" variant="body2">
+            {"Create your account,"}
+          </Link>{" "}
+          it takes less than a minute.
+        </Typography>
+      </Grid>
+      <form className={classes.form} noValidate>
+        <TextField
+          variant="standard"
+          margin="normal"
+          required
+          fullWidth
+          id="username"
+          label="Username"
+          name="username"
+          autoComplete="username"
+          autoFocus
+        />
+        <TextField
+          variant="standard"
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+        />
+        <Grid
+          container
+          direction="row"
+          justify="space-between"
+          alignItems="center"
+          className={classes.rememberMeContainer}
+        >
+          <Grid item>
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+          </Grid>
+
+          <Grid item>
+            <Link href="#" variant="body2">
+              Forgot password?
+            </Link>
+          </Grid>
+        </Grid>
+        <Box mt={5}></Box>
+        <Fab
+          variant="extended"
+          aria-label="like"
+          color="primary"
+          type="submit"
+          size="large"
+          className={classes.loginFab}
+        >
+          Login
+        </Fab>
+      </form>
+
+      <Grid
+        container
+        direction="row"
+        justify="space-evenly"
+        className={classes.socialLoginContainer}
+      >
+        <Grid item>
+          <Typography variant="subtitle1" className={classes.loginCaption}>
+            Or login with
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Fab variant="extended" aria-label="like" color="primary">
+            <img
+              src={Facebook}
+              alt="facebook-logo"
+              className={classes.loginFabIcon}
+            />
+          </Fab>
+        </Grid>
+        <Grid item>
+          <Fab variant="extended" aria-label="like" color="primary">
+            <img
+              src={Twitter}
+              alt="twitter-logo"
+              className={classes.loginFabIcon}
+            />
+          </Fab>
+        </Grid>
+        <Grid item>
+          <Fab variant="extended" aria-label="like" color="primary">
+            <img
+              src={Google}
+              alt="google-plus-logo"
+              className={classes.loginFabIcon}
+            />
+          </Fab>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default LoginForm;
