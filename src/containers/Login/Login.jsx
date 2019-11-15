66da5881b45be7c076450a679505767a9f90dc3f
ihapmustapha <ihapmustapha@gmail.com>
2019-11-15 21:41:14 +0200

    Login Component

---
+++ src/containers/Login/Login.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import useStyles from "./styles";
+import LoginForm from "../../components/LoginForm/LoginForm";
+
+const Login = props => {
+  const classes = useStyles();
+
+  return (
+    <Grid
+      container
+      component="main"
+      className={classes.root}
+      direction="column"
+      justify="center"
+      alignItems="center"
+    >
+      {/* Login Form */}
+      <Grid
+        container
+        className={classes.loginCardContainer}
+        item
+        xs={8}
+        sm={8}
+        md={8}
+        component={Paper}
+        elevation={6}
+        square
+      >
+        <Grid item xs={12} sm={12} md={6}>
+          <LoginForm classes={classes} />
+        </Grid>
+        <Grid container item direction="column" xs={false} sm={false} md={6}>
+          <Grid item md={12} className={classes.cardImage}>
+            <Grid
+              container
+              direction="column"
+              justify="center"
+              alignItems="center"
+            >
+              <Grid item>
+                <Typography>Welcome</Typography>
+              </Grid>
+
+              <Grid item>
+                <Typography>
+                  {`Sed do eiusmod temporut laboure et dolore magna eliqua. Your perfect place to buy & sell`}
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Login;
