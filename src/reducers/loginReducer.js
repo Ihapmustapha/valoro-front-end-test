909f4917b10922a5c7fcdfde36fefc7aa9a82bb7
ihapmustapha <ihapmustapha@gmail.com>
2019-11-16 19:47:20 +0200

    adding login reducer logic

--- src/reducers/loginReducer.js
+++ src/reducers/loginReducer.js
@@ -1,15 +1,34 @@
 import * as actionTypes from "../actions/actionTypes";
 
-const initState = {};
+const initState = {
+  loginIsLoading: false,
+  loginToken: "",
+  loginError: null
+};
 
 const loginReducer = (state = initState, action) => {
   switch (action.type) {
     case actionTypes.LOGIN_TRIGGER:
-      return { ...state };
+      return {
+        ...state,
+        loginIsLoading: true,
+        loginToken: "",
+        loginError: null
+      };
     case actionTypes.LOGIN_SUCCESS:
-      return { ...state };
+      return {
+        ...state,
+        loginIsLoading: false,
+        loginToken: action.payload,
+        loginError: null
+      };
     case actionTypes.LOGIN_FAIL:
-      return { ...state };
+      return {
+        ...state,
+        loginIsLoading: false,
+        loginToken: "",
+        loginError: action.error
+      };
 
     default:
       return state;
