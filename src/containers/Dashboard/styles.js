da69cd9da4024a42022fc42ab3b91ee84a5941ee
ihapmustapha <ihapmustapha@gmail.com>
2019-11-18 13:28:51 +0200

    adding dashboardContainer Style

--- src/containers/Dashboard/styles.js
+++ src/containers/Dashboard/styles.js
@@ -1,5 +1,9 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles(theme => ({}));
+const useStyles = makeStyles(theme => ({
+  dashboardElementContainer: {
+    marginLeft: "280px"
+  }
+}));
 
 export default useStyles;
