f7f7a5997d1f488a7d2d26dbca934fbe3cb881e6
ihapmustapha <ihapmustapha@gmail.com>
2019-11-18 12:15:42 +0200

    removing unnecessary Async Await from loginService

--- src/services/login.js
+++ src/services/login.js
@@ -2,7 +2,7 @@ import Axios from "axios";
 
 const API_URL = `https://reqres.in/api/login`;
 
-const loginService = async (email, password) =>
-  await Axios.post(API_URL, { email, password });
+const loginService = (email, password) =>
+  Axios.post(API_URL, { email, password });
 
 export default loginService;
