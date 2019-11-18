9c4187224ac63912f1587fa26048033ac74f8e12
ihapmustapha <ihapmustapha@gmail.com>
2019-11-18 13:28:10 +0200

    binding Dashboard to fetchAction and Store

--- src/containers/Dashboard/Dashboard.jsx
+++ src/containers/Dashboard/Dashboard.jsx
@@ -1,17 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
 import useStyles from "./styles";
 import Topbar from "../../components/layout/Topbar/Topbar";
 import SideDrawer from "../../components/layout/SideDrawer/SideDrawer";
+import DashboardItem from "../../components/layout/DashboardItem/DashboardItem";
+import { connect } from "react-redux";
+import * as actions from "../../actions/index";
 
-const Dashboard = props => {
+const Dashboard = ({ fetchDashboard, dashboardData }) => {
   const classes = useStyles();
+
+  useEffect(() => {
+    fetchDashboard();
+  }, []);
+
+  console.log(dashboardData);
 
   return (
     <>
       <Topbar />
       <SideDrawer />
+      <div className={classes.dashboardElementContainer}>
+        {dashboardData &&
+          dashboardData.map(elementData => (
+            <DashboardItem key={elementData.id} elementData={elementData} />
+          ))}
+      </div>
     </>
   );
 };
 
-export default Dashboard;
+const mapStateToProps = state => ({
+  dashboardData:
+    state.dashboardReducer &&
+    state.dashboardReducer.dashboardData &&
+    state.dashboardReducer.dashboardData &&
+    state.dashboardReducer.dashboardData.data &&
+    state.dashboardReducer.dashboardData.data.data
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchDashboard: () => dispatch(actions.fetchDashboard())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
