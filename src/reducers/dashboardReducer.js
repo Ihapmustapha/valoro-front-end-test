4acb015cd0ef24a8126954fcf14e89910decd46d
ihapmustapha <ihapmustapha@gmail.com>
2019-11-18 12:34:41 +0200

    maintaining dashboardReducer

--- src/reducers/dashboardReducer.js
+++ src/reducers/dashboardReducer.js
@@ -1,15 +1,34 @@
 import * as actionTypes from "../actions/actionTypes";
 
-const initState = {};
+const initState = {
+  dashboardIsLoading: false,
+  dashboardData: null,
+  dashboardError: null
+};
 
 const dashboardReducer = (state = initState, action) => {
   switch (action.type) {
     case actionTypes.FETCH_DASHBOARD_TRIGGER:
-      return { ...state };
+      return {
+        ...state,
+        dashboardIsLoading: true,
+        dashboardData: null,
+        dashboardError: null
+      };
     case actionTypes.FETCH_DASHBOARD_SUCCESS:
-      return { ...state };
+      return {
+        ...state,
+        dashboardIsLoading: false,
+        dashboardData: action.payload,
+        dashboardError: null
+      };
     case actionTypes.FETCH_DASHBOARD_FAIL:
-      return { ...state };
+      return {
+        ...state,
+        dashboardIsLoading: false,
+        dashboardData: null,
+        dashboardError: action.error
+      };
 
     default:
       return state;
